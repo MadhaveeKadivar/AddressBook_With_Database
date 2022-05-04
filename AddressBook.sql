461c4e9b7385200f8bbd80d7c7bf01613f3922b3
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-04 18:45:34 +0530

    [Madhavee] retrieve data in order from table

--- AddressBook.sql
+++ AddressBook.sql
@@ -25,8 +25,6 @@ select * from AddressBookTable;
 ---UC_5 Delete person from table
 delete from AddressBookTable where First_name = 'Akanksha';
 
-
-
 ----UC_6 Retrieve data using City name
 insert into AddressBookTable
 values ('Abhilash','Meher','sundergarth','Bhuvneswar','Odisha',845623,84512845554,'abhilash@gmail.com'),
@@ -41,3 +39,11 @@ select count(city) as Number_of_City from AddressBookTable ;
 select count(State) as Number_of_States from AddressBookTable ;
 select City,count(First_name) as TotalPersons from AddressBookTable group by City;
 select State,count(First_name) as TotalPersons from AddressBookTable group by State;
+
+---UC_8 retrieve entries sorted alphabetically by Person’s name for a given city
+insert into AddressBookTable
+values ('Lisan','Kadivar','Jamjodhpur','Jamnagar','Gujarat',360530,98461532455,'lisan@gmail.com'),
+('Brich','Dharsandia','Morbi','Morbi','Gujarat',845185,8745184585,'brich@gmail.com'),
+('Princy','Chhatrala','Junagadh','Junagadh','Gujarat',845896,9864518745,'princy@gmail.com');
+select * from AddressBookTable;
+select * from AddressBookTable where City = 'Jamnagar' order by First_name;
