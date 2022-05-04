fe17d66600fc22ae5abd720d968d74dc85072254
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-04 17:07:29 +0530

    [Madhavee] Create AddressBookTable

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,14 @@
 ---UC_1 Create a Address Book Service DB
 create database AddressBookService;
+use AddressBookService;
+
+---UC_2 create a AddressBook Table
+create table AddressBookTable(
+First_name varchar(255),
+Last_name varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+Zip int,
+Phone_number bigint,
+Email varchar(255));
