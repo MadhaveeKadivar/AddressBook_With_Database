fd4574fe75efb8e32adc9a27c283f3ae70d12c64
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-04 17:38:44 +0530

    [Madhavee] update existing person

--- AddressBook.sql
+++ AddressBook.sql
@@ -12,3 +12,12 @@ State varchar(255),
 Zip int,
 Phone_number bigint,
 Email varchar(255));
+
+---UC_3 Insert Data In Table
+insert into AddressBookTable
+values ('Madhavee','Kadivar','Jamjodhpur','Jamnagar','Gujarat',360530,8562451251,'mk@gmail.com'),
+('Akanksha','Lanjewar','Nagpur','Nagpur','Maharastra',561235,87451284512,'akanksha@gmail.com');
+
+---UC_4 Edit the existing contact
+update AddressBookTable set Zip = 985625 where First_name = 'Akanksha';
+select * from AddressBookTable;
